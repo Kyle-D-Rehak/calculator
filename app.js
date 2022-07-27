9dfe7b1003139c6f177e72a86803f974174f74f0
Kyle Rehak <kylerehak@gmail.com>
2022-07-27 16:20:52 +0800

    Add storedValue display and answer rounding

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 let displayValue = '';
 let storedValue;
 let operator;
-const display = document.querySelector('.display');
+const display = document.querySelector('.currentValue');
+const storage = document.querySelector('.storage');
 const numButtons = Array.from(document.querySelectorAll('.num-button'));
 const clearButtons = Array.from(document.querySelectorAll('.clear-button'));
 const opButtons = Array.from(document.querySelectorAll('.op-button'));
@@ -19,10 +20,12 @@ function clear(e) {
     if (e.currentTarget.value === 'clear') {
         displayValue = '';
         display.textContent = displayValue;
+        storage.textContent = storedValue;
     } else if (e.currentTarget.value === 'clear-all') {
         displayValue = '';
         storedValue = '';
         operator = '';
+        storage.textContent = storedValue;
         display.textContent = displayValue;
     }
 }
@@ -30,15 +33,19 @@ function clear(e) {
 function runOperation(e) {
     if (displayValue === 'ERROR') {
         displayValue = storedValue;
-        sotredValue = '';
+        storedValue = '';
+        storage.textContent = storedValue;
         display.textContent = displayValue;
         return;
     } else if (e.currentTarget.value === 'equals') {
+        storage.textContent = '';
         solve(operator, storedValue, +displayValue);
+        storedValue = '';
     } else {
     operator = e.currentTarget.value;
     storedValue = +displayValue;
     displayValue = '';
+    storage.textContent = `${storedValue} ${operator}`;
     display.textContent = displayValue;
     }
 }
@@ -64,27 +71,33 @@ const divide = function (a, b) {
 
 const solve = function (operation, a, b) {
     switch(operation) {
-        case 'add':
+        case '+':
             displayValue = add(a, b);
-            operator = '';
-            display.textContent = displayValue;
+
             break;
-        case 'subtract':
+        case '-':
             displayValue = subtract(a, b);
-            operator = '';
-            display.textContent = displayValue;
             break;
-        case 'multiply':
+        case 'x':
             displayValue = multiply(a, b);
-            operator = '';
-            display.textContent = displayValue;
             break;
-        case 'divide':
+        case '/':
             displayValue = divide(a, b);
-            operator = '';
-            display.textContent = displayValue;
             break;
         default:
             break;
     }
+
+    if (displayValue.toString().length > 15) {
+        if (/e/.test(displayValue.toString())) {
+            display.textContent = displayValue.toPrecision(9);
+        } else {
+        display.textContent = displayValue.toPrecision(14);
+        }
+        storage.textContent = '(ANS ROUNDED)';
+    } else {
+        display.textContent = displayValue;
+    }
+
+    operator = '';
 }
